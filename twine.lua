925bf0e8a9061ad824d0557528e31ad43144065e
Colin Drake <github@accounts.colindrake.me>
2021-02-13 00:48:56 +0700

    updates...

--- twine.lua
+++ twine.lua
@@ -1,288 +1,235 @@
 -- twine
--- to form by twisting,
+-- "to form by twisting,
 -- intertwining,
--- or interlacing...
+-- or interlacing..."
 --
 -- by: @cfd90
 --
 -- ENC1 volume
--- KEY2 randomize pads 1
--- KEY3 randomize pads 2
+-- KEY2 randomize 1
+-- KEY3 randomize 2
 -- ENC2 seek 1
 -- ENC3 seek 2
 
 engine.name = "Glut"
 
 local g
-local matrix = {}
+local x = 1
+local y = 1
 
-local selx = 1
-local sely = 1
-
-local sk1 = 0
-local sk2 = 0
-
-function init()
-  params:add_separator()
-
-  params:add_file("sample", "1 sample")
-  params:set_action("sample", function(file) engine.read(1, file) end)
+local function setup_params()
+  params:add_separator("reverb")
   
-  params:add_file("2sample", "2 sample")
-  params:set_action("2sample", function(file) engine.read(2, file) end)
+  params:add_taper("reverb_mix", "* mix", 0, 100, 50, 0, "%")
+  params:set_action("reverb_mix", function(value) engine.reverb_mix(value / 100) end)
+
+  params:add_taper("reverb_room", "* room", 0, 100, 50, 0, "%")
+  params:set_action("reverb_room", function(value) engine.reverb_room(value / 100) end)
+
+  params:add_taper("reverb_damp", "* damp", 0, 100, 50, 0, "%")
+  params:set_action("reverb_damp", function(value) engine.reverb_damp(value / 100) end)
   
-  params:add_taper("volume", "1 volume", -60, 20, 0, 0, "dB")
-  params:set_action("volume", function(value) engine.volume(1, math.pow(10, value / 20)) end)
-
-  params:add_taper("speed", "1 speed", -400, 400, 0, 0, "%")
-  params:set_action("speed", function(value) engine.speed(1, value / 100) end)
-
-  params:add_taper("jitter", "1 jitter", 0, 500, 0, 5, "ms")
-  params:set_action("jitter", function(value) engine.jitter(1, value / 1000) end)
-
-  params:add_taper("size", "1 size", 1, 500, 100, 5, "ms")
-  params:set_action("size", function(value) engine.size(1, value / 1000) end)
-
-  params:add_taper("density", "1 density", 0, 512, 20, 6, "hz")
-  params:set_action("density", function(value) engine.density(1, value) end)
-
-  params:add_taper("pitch", "1 pitch", -48, 48, 0, 0, "st")
-  params:set_action("pitch", function(value) engine.pitch(1, math.pow(0.5, -value / 12)) end)
-
-  params:add_taper("spread", "1 spread", 0, 100, 0, 0, "%")
-  params:set_action("spread", function(value) engine.spread(1, value / 100) end)
-
-  params:add_taper("2volume", "2 volume", -60, 20, 0, 0, "dB")
-  params:set_action("2volume", function(value) engine.volume(2, math.pow(10, value / 20)) end)
-
-  params:add_taper("2speed", "2 speed", -400, 400, 0, 0, "%")
-  params:set_action("2speed", function(value) engine.speed(2, value / 100) end)
-
-  params:add_taper("2jitter", "2 jitter", 0, 500, 0, 5, "ms")
-  params:set_action("2jitter", function(value) engine.jitter(2, value / 1000) end)
-
-  params:add_taper("2size", "2 size", 1, 500, 100, 5, "ms")
-  params:set_action("2size", function(value) engine.size(2, value / 1000) end)
-
-  params:add_taper("2density", "2 density", 0, 512, 20, 6, "hz")
-  params:set_action("2density", function(value) engine.density(2, value) end)
-
-  params:add_taper("2pitch", "2 pitch", -48, 48, 0, 0, "st")
-  params:set_action("2pitch", function(value) engine.pitch(2, math.pow(0.5, -value / 12)) end)
-
-  params:add_taper("2spread", "2 spread", 0, 100, 0, 0, "%")
-  params:set_action("2spread", function(value) engine.spread(2, value / 100) end)
-
-  params:add_taper("2fade", "2 att / dec", 1, 9000, 1000, 3, "ms")
-  params:set_action("2fade", function(value) engine.envscale(2, value / 1000) end)
+  params:add_separator("samples")
   
-  params:hide("volume")
-  params:hide("speed")
-  params:hide("jitter")
-  params:hide("size")
-  params:hide("density")
-  params:hide("pitch")
-  params:hide("spread")
-  params:hide("fade")
-  params:hide("2volume")
-  params:hide("2speed")
-  params:hide("2jitter")
-  params:hide("2size")
-  params:hide("2density")
-  params:hide("2pitch")
-  params:hide("2spread")
-  params:hide("2fade")
+  for i=1,2 do
+    params:add_file(i .. "sample", i .. " sample")
+    params:set_action(i .. "sample", function(file) engine.read(i, file) end)
+    
+    params:add_taper(i .. "volume", i .. " volume", -60, 20, 0, 0, "dB")
+    params:set_action(i .. "volume", function(value) engine.volume(i, math.pow(10, value / 20)) end)
+  
+    params:add_taper(i .. "speed", i .. " speed", -400, 400, 0, 0, "%")
+    params:set_action(i .. "speed", function(value) engine.speed(i, value / 100) end)
+  
+    params:add_taper(i .. "jitter", i .. " jitter", 0, 500, 0, 5, "ms")
+    params:set_action(i .. "jitter", function(value) engine.jitter(i, value / 1000) end)
+  
+    params:add_taper(i .. "size", i .. " size", 1, 500, 100, 5, "ms")
+    params:set_action(i .. "size", function(value) engine.size(i, value / 1000) end)
+  
+    params:add_taper(i .. "density", i .. " density", 0, 512, 20, 6, "hz")
+    params:set_action(i .. "density", function(value) engine.density(i, value) end)
+  
+    params:add_taper(i .. "pitch", i .. " pitch", -48, 48, 0, 0, "st")
+    params:set_action(i .. "pitch", function(value) engine.pitch(i, math.pow(0.5, -value / 12)) end)
+  
+    params:add_taper(i .. "spread", i .. " spread", 0, 100, 0, 0, "%")
+    params:set_action(i .. "spread", function(value) engine.spread(i, value / 100) end)
+    
+    params:add_taper(i .. "fade", i .. " att / dec", 1, 9000, 1000, 3, "ms")
+    params:set_action(i .. "fade", function(value) engine.envscale(i, value / 1000) end)
+    
+    params:add_taper(i .. "seek", i .. " seek", 0, 100, 0, 0, "%")
+    params:set_action(i .. "seek", function(value) engine.seek(i, value / 100) end)
+    
+    params:hide(i .. "volume")
+    params:hide(i .. "speed")
+    params:hide(i .. "jitter")
+    params:hide(i .. "size")
+    params:hide(i .. "density")
+    params:hide(i .. "pitch")
+    params:hide(i .. "spread")
+    params:hide(i .. "fade")
+    params:hide(i .. "seek")
+  end
 
   params:bang()
-  
+end
+
+local function grid_redraw()
+  g:all(0)
+  g:led(x, y, 15)
+  g:refresh()
+end
+
+local function setup_grid()
   g = grid.connect()
   g.key = grid_key
+end
+
+local function randomize(n)
+  local jitter = math.random(0, 500)
+  local size = math.random(0, 500)
+  local density = math.random(1, 20)
+  local spread = math.random(0, 100)
+  local pitches = {-12, -5, 0, 12, 7,}
+  local pitch_idx = math.random(1, #pitches)
+  local pitch = pitches[pitch_idx]
   
-  for x=1,g.rows do
-    matrix[x] = {}
-    
-    for y=1,g.cols do
-      matrix[x][y] = {}
-    end
-  end
-  
-  randomize(1)
-  randomize(2)
-  
-  grid_redraw()
-  
+  params:set(n .. "jitter", jitter)
+  params:set(n .. "size", size)
+  params:set(n .. "density", density)
+  params:set(n .. "spread", spread)
+  params:set(n .. "pitch", pitch)
+end
+
+local function setup_engine()
   engine.seek(1, 0)
   engine.gate(1, 1)
   
   engine.seek(2, 0)
   engine.gate(2, 1)
-  
-  apply(selx, sely)
+
+  randomize(1)
+  randomize(2)
 end
 
-function randomize(n)
-  for x=1,g.rows do
-    for y=1,g.cols do
-      local jitter = math.random(0, 500)
-      local size = math.random(0, 500)
-      local density = math.random(1, 20)
-      local spread = math.random(0, 100)
-      local pitches = {-12, -5, 0, 12, 7,}
-      local pitch_idx = math.random(1, #pitches)
-      local pitch = pitches[pitch_idx]
-      
-      if n == 1 then
-        matrix[x][y].jitter = jitter
-        matrix[x][y].size = size
-        matrix[x][y].density = density
-        matrix[x][y].spread = spread
-        matrix[x][y].pitch = pitch
-        sk1 = 0
-      elseif n == 2 then
-        matrix[x][y].jitter2 = jitter
-        matrix[x][y].size2 = size
-        matrix[x][y].density2 = density
-        matrix[x][y].spread2 = spread
-        matrix[x][y].pitch2 = pitch
-        sk2 = 0
-      end
-    end
+local function round(val, decimal)
+  if (decimal) then
+    return math.floor( (val * 10^decimal) + 0.5) / (10^decimal)
+  else
+    return math.floor(val+0.5)
   end
 end
 
-function apply(x, y)
-  local m = matrix[x][y]
-  
-  params:set("jitter", m.jitter)
-  params:set("size", m.size)
-  params:set("density", m.density)
-  params:set("spread", m.spread)
-  params:set("pitch", m.pitch)
-  
-  params:set("2jitter", m.jitter2)
-  params:set("2size", m.size2)
-  params:set("2density", m.density2)
-  params:set("2spread", m.spread2)
-  params:set("2pitch", m.pitch2)
+function init()
+  setup_params()
+  setup_grid()
+  setup_engine()
+  grid_redraw()
 end
 
-function grid_key(x, y, z)
-  if z == 1 then
-    selx = x
-    sely = y
-    
-    local pct = (((y-1)*g.rows) + x) / (g.rows * g.cols)
-    engine.seek(1, pct)
-    engine.seek(2, pct)
-    
-    sk1 = 0
-    sk2 = 0
-    
-    apply(x, y)
+function grid_key(_x, _y, z)
+  if z == 0 then
+    return
   end
+  
+  x = _x
+  y = _y
+
+  local grid_size = g.rows * g.cols
+  local this_idx = ((_y - 1) * g.rows) + _x
+  local pct = this_idx / grid_size
+  
+  params:set("1seek", pct * 100)
+  params:set("2seek", pct * 100)
   
   grid_redraw()
   redraw()
 end
 
-function grid_redraw()
-  g:all(0)
-  g:led(selx, sely, 15)
-  g:refresh()
-end
-
 function redraw()
-  local m = matrix[selx][sely]
-  
   screen.clear()
   screen.move(0, 10)
   screen.level(15)
   screen.text("jitter: ")
   screen.level(5)
-  screen.text(m.jitter)
+  screen.text(params:get("1jitter"))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(m.jitter2)
+  screen.text(params:get("2jitter"))
   screen.move(0, 20)
   screen.level(15)
   screen.text("size: ")
   screen.level(5)
-  screen.text(m.size)
+  screen.text(params:get("1size"))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(m.size2)
+  screen.text(params:get("2size"))
   screen.move(0, 30)
   screen.level(15)
   screen.text("density: ")
   screen.level(5)
-  screen.text(m.density)
+  screen.text(params:get("1density"))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(m.density2)
+  screen.text(params:get("2density"))
   screen.move(0, 40)
   screen.level(15)
   screen.text("spread: ")
   screen.level(5)
-  screen.text(m.spread)
+  screen.text(params:get("1spread"))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(m.spread2)
+  screen.text(params:get("2spread"))
   screen.move(0, 50)
   screen.level(15)
   screen.text("pitch: ")
   screen.level(5)
-  screen.text(m.pitch)
+  screen.text(params:get("1pitch"))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(m.pitch2)
+  screen.text(params:get("2pitch"))
   screen.move(0, 60)
   screen.level(15)
-  screen.text("seek offset: ")
+  screen.text("seek: ")
   screen.level(5)
-  screen.text(sk1)
+  screen.text(round(params:get("1seek"), 2))
   screen.level(1)
   screen.text(" / ")
   screen.level(5)
-  screen.text(sk2)
+  screen.text(round(params:get("2seek"), 2))
   screen.update()
 end
 
 function enc(n, d)
   if n == 1 then
-    params:delta("volume", d)
+    params:delta("1volume", d)
     params:delta("2volume", d)
   elseif n == 2 then
-    if d > 0 then
-      sk1 = sk1 + 0.01
-    elseif d < 0 then
-      sk1 = sk1 - 0.01
-    end  
+    params:delta("1seek", d)
   elseif n == 3 then
-    if d > 0 then
-      sk2 = sk2 + 0.01
-    elseif d < 0 then
-      sk2 = sk2 - 0.01
-    end  
+    params:delta("2seek", d)
   end
-  
-  local pct = ((sely*g.rows) + selx) / (g.rows * g.cols)
-  engine.seek(1, pct + sk1)
-  engine.seek(2, pct + sk2)
   
   redraw()
 end
 
 function key(n, z)
-  if n == 2 and z == 1 then
+  if z == 0 then
+    return
+  end
+  
+  if n == 2 then
     randomize(1)
-  elseif n == 3 and z == 1 then
+  elseif n == 3 then
     randomize(2)
   end
-  apply(selx, sely)
+  
   redraw()
 end
